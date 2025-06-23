ec9ea64a74080c1dadc36324cf6cc8a715103639
Jason Zubiate <jzubiate.dev@gmail.com>
2025-06-23 10:54:44 -0700

    adds motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
+import { motion } from "motion/react";
 
 export default function Home() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <main className="relative h-screen w-screen">
+    <main className="relative h-screen w-screen overflow-hidden">
       <video
         src="/video-01.mp4"
         autoPlay
@@ -12,8 +18,19 @@ export default function Home() {
         className="absolute w-screen h-screen object-cover"
       ></video>
 
-      <div className="absolute left-8 right-8 top-1/2 -translate-y-1/2 bg-zinc-950/80 rounded-xl p-4 lg:p-6 flex flex-col">
-        <div className="h-[clamp(100px,80vw,600px)] w-full rounded-lg overflow-hidden relative mb-6">
+      <motion.div
+        animate={
+          isMenuOpen
+            ? { y: "10%", scale: 0.95, opacity: 0 }
+            : { y: "0%", scale: 1, opacity: 1 }
+        }
+        transition={{
+          duration: 0.8,
+          ease: [0.83, 0, 0.17, 1],
+        }}
+        className="absolute left-8 right-8 top-1/2 -translate-y-1/2 bg-zinc-950/80 rounded-2xl p-6 flex flex-col"
+      >
+        <div className="h-[clamp(100px,80vw,600px)] w-full rounded-xl overflow-hidden relative mb-6">
           <Image
             src="/image-01.jpeg"
             fill
@@ -31,10 +48,50 @@ export default function Home() {
           </p>
         </div>
 
-        <button className="w-full rounded-full py-4 bg-zinc-200/10 text-zinc-50 font-semibold uppercase border border-zinc-200/10 hover:bg-zinc-200/20 transition-colors duration-300 cursor-pointer">
+        <button
+          onClick={() => setIsMenuOpen(true)}
+          className="w-full rounded-full py-4 bg-zinc-200/10 text-zinc-50 font-semibold uppercase border border-zinc-200/10 hover:bg-zinc-200/20 transition-colors duration-300 cursor-pointer"
+        >
           Pre-Save
         </button>
-      </div>
+      </motion.div>
+
+      <Menu isMenuOpen={isMenuOpen} />
     </main>
+  );
+}
+
+function Menu({ isMenuOpen }: { isMenuOpen: boolean }) {
+  const menuItems = [
+    "Spotify",
+    "Apple Music",
+    "SoundCloud",
+    "YouTube Music",
+    "Deezer",
+    "Amazon Music",
+    "Tidal",
+  ];
+  return (
+    <motion.ul
+      animate={
+        isMenuOpen
+          ? { y: "-100%", scale: 1, opacity: 1 }
+          : { y: "0%", scale: 0.95, opacity: 0 }
+      }
+      transition={{
+        duration: 0.8,
+        ease: [0.83, 0, 0.17, 1],
+      }}
+      className="absolute left-8 right-8 bottom-8 translate-y-[110%] bg-zinc-950/80 rounded-2xl p-4 flex flex-col gap-2"
+    >
+      {menuItems.map((item, index) => (
+        <motion.li
+          key={index}
+          className="w-full py-2 px-4 rounded-xl bg-zinc-200/10 text-zinc-50 font-semibold uppercase border border-zinc-200/10 hover:bg-zinc-200/20 transition-colors duration-300 cursor-pointer"
+        >
+          {item}
+        </motion.li>
+      ))}
+    </motion.ul>
   );
 }
